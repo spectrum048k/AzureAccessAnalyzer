c017bb1ff7f895322740650d48dfef553c8f501e
spectrum048k <>
2023-05-22 22:21:51 +1200

    add command line arguments

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import traceback
 import az
 import os
 import sys
@@ -8,47 +9,75 @@ from loguru import logger
 def format_object(obj):
     return json.dumps(obj, indent=4)
 
-def main():
-    # use json format for the logging to stdout and stderr
-    # logger.remove()
-    # logger.add(sys.stdout, format="{time} {level} {message}", level="DEBUG", serialize=True)
-    logger.info("Starting up...")
+def validate_arguments():
+    # Get the command-line arguments
+    args = sys.argv
+    logger.debug(f'Command-line arguments: {args}')
 
-    auth_headers = az.get_token_header(
+    if len(args) < 3:
+        raise Exception('Subscription ID and user name are a required argument')
+    
+    sub_id = args[1]
+    user_name = args[2]
+
+    # check if there is a third argument num hours which should be a positive integer
+    if len(args) > 3:
+        try:
+            num_hours = int(args[3])
+            if num_hours < 1:
+                raise ValueError('num_hours must be a positive integer')
+            
+        except ValueError:
+            raise Exception('num_hours must be a positive integer')
+    else:
+        logger.debug('num_hours not specified, defaulting to 1')
+        num_hours = 1
+
+    # check fourth optional arument rg_name
+    if len(args) > 4:
+        rg_name = args[4]
+    else:
+        logger.debug('rg_name not specified, defaulting to None')
+        rg_name = None
+
+    return sub_id, user_name, num_hours, rg_name
+
+def main():
+    try:
+        logger.info("Starting up...")
+
+        sub_id, user_name, num_hours, rg_name = validate_arguments()
+
+        auth_headers = az.get_token_header(
         os.environ.get("TENANT_ID"), 
         os.environ.get("CLIENT_ID"), 
         os.environ.get("CLIENT_SECRET"))
 
-    azure = az.AzureAPI(auth_headers)
+        azure = az.AzureAPI(auth_headers)
 
-    subs = azure.get_subscriptions()
-    logger.debug(f'subscriptions:')
-    logger.debug(format_object(subs))
+        start_date, end_date = datetime_helper.get_last_n_hours(num_hours)
+        select = None
 
-    sub_id = subs[0]['subscriptionId']
+        activity_log = azure.get_activity_log(sub_id, start_date, end_date, rg_name, select, user_name)
 
-    rg = 'rg-app-a-temp'
-    user_name = 'usera@eoghankennyoutlook.onmicrosoft.com'
-    start_date, end_date = datetime_helper.get_last_week()
-    select = None
+        logger.debug(f'activity log response:')
+        logger.debug(format_object(activity_log))
 
-    activity_log = azure.get_activity_log(sub_id, start_date, end_date, rg, select, user_name)
+        # extract the operation values
+        operations = [x['operationName']['value'] for x in activity_log['value']]
 
-    logger.debug(f'activity log response:')
-    logger.debug(format_object(activity_log))
+        logger.debug(f'List of operations:')
+        logger.debug(format_object(operations))
 
-    # extract the operation values
-    operations = [x['operationName']['value'] for x in activity_log['value']]
+        # sort and remove duplicates
+        operations = sorted(list(set(operations)))
 
-    logger.debug(f'List of operations:')
-    logger.debug(format_object(operations))
-
-    # sort and remove duplicates
-    operations = sorted(list(set(operations)))
-
-    logger.info(f'List of operations for {user_name} between {start_date} and {end_date}:')
-    logger.info(format_object(operations))
-
+        logger.info(f'List of operations for {user_name} between {start_date} and {end_date}:')
+        logger.info(format_object(operations))
+    except Exception as e:
+        logger.error(f"Unexpected exception occurred: {e}")
+        logger.error(f"Stack trace: {traceback.print_exc()}")
+    
 # Check if the script is being run directly (not imported as a module)
 if __name__ == "__main__":
     main()
