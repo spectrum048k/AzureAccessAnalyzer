4f3518cce2418094d0c8afcdadd1f232f276b43e
spectrum048k <107911114+spectrum048k@users.noreply.github.com>
2023-05-27 16:22:40 +1200

    Feature/check assignments (#2)
    azure api class refactoring
    
    ---------
    
    Co-authored-by: spectrum048k <>

--- src/azure_api.py
+++ src/azure_api.py
@@ -1,5 +1,6 @@
-import datetime
 import http
+import json
+import os
 import requests
 from loguru import logger
 
@@ -9,64 +10,78 @@ class AzureAPI():
     AZURE_REST_API_BASE_URL = "https://management.azure.com"
 
     def log_requests(self, *args):
-        logger.debug(f"Request: {args}")
+        logger.debug(f"Request {args}")
 
-    def __init__(self, auth_headers):
-        self.auth_headers = auth_headers
+    def __init__(self):
         http.client.HTTPConnection.debuglevel = 1
+        http.client.HTTPSConnection.debuglevel = 1
         http.client.print = self.log_requests
     
-    def get_subscriptions(self):
-        """ Get the list of subscriptions """
-        url = f"{self.AZURE_REST_API_BASE_URL}/subscriptions?api-version=2021-01-01"
-        response = requests.get(url, headers=self.auth_headers)
-        return response.json()['value']
-    
-    def get_resource_groups(self, subscription_id):
-        """ Get the list of resource groups """
-        # validate subscription_id is a valid GUID
-        self.is_valid_guid(subscription_id)
-
-        # build the url using base url, subscription_id and api version
-        url = f"{self.AZURE_REST_API_BASE_URL}/subscriptions/{subscription_id}/resourcegroups?api-version=2020-06-01"
-        response = requests.get(url, headers=self.auth_headers)
-        return response.json()
-    
-    def get_activity_log(self, subscription_id:str, start_date:datetime, end_date:datetime,
-                         resource_group=None, select=None, userName=None):
-        """ Get the activity log for a subscription between two dates"""
-
-        # validate subscription_id is a valid GUID
-        self.is_valid_guid(subscription_id)
-        
-        # validate start_date is before end_date
-        if start_date > end_date:
-            raise Exception('start_date must be before end_date')
-        
-        # convert the dates to utc and to iso format
-        start_date = start_date.astimezone(datetime.timezone.utc).strftime("%Y-%m-%dT%H:%M:%S")
-        end_date = end_date.astimezone(datetime.timezone.utc).strftime("%Y-%m-%dT%H:%M:%S")
-
-        filter = f"$filter=eventTimestamp ge '{start_date}' and eventTimestamp le '{end_date}'"
-
-        if resource_group:
-            filter += f" and resourceGroupName eq '{resource_group}'"
-
-        if userName:
-            filter += f" and caller eq '{userName}'"
-
-        if select:
-            filter += f"&$select={select}"
-
-        url = f"{self.AZURE_REST_API_BASE_URL}/subscriptions/{subscription_id}/providers/microsoft.insights/eventtypes/management/values?api-version=2017-03-01-preview&{filter}&{select}"
-
-        response = requests.get(url, headers=self.auth_headers)
-
-        if response.status_code == 200:
-            return response.json()
-        else:
-            raise Exception(f"Error getting activity log: {response.text}")
+    def format_json_object(self, obj):
+        return json.dumps(obj, indent=4)
 
     def is_valid_guid(self, guid):
-        if not guid or not isinstance(guid, str) or not len(guid) == 36:
-            raise Exception('value must be a valid GUID')
+        if not guid or not isinstance(guid, str) or len(guid) != 36:
+            raise ValueError('value must be a valid GUID')
+        
+    def get_token_header(self):
+        """Gets an access token using the client credentials flow"""
+
+        tenant_id = os.environ.get("TENANT_ID")
+        client_id = os.environ.get("CLIENT_ID")
+        client_secret = os.environ.get("CLIENT_SECRET")
+
+        # check variables are set
+        if not tenant_id and not AzureAPI.is_valid_guid(tenant_id):
+            raise ValueError('TENANT_ID environment variable is not set or invalid.')
+        if not client_id and not AzureAPI.is_valid_guid(client_id):
+            raise ValueError('CLIENT_ID environment variable is not set or invalid.')
+        if not client_secret:
+            raise ValueError('CLIENT_SECRET environment variable is not set.')
+
+        # Get an access token using the client credentials flow
+        url = f'https://login.microsoftonline.com/{tenant_id}/oauth2/v2.0/token'
+        data = {
+            'client_id': client_id,
+            'client_secret': client_secret,
+            'grant_type': 'client_credentials',
+            'scope': 'https://management.azure.com/.default'
+        }
+
+        response = requests.post(url, data=data)
+
+        self.log_response_info(response)
+
+        access_token = response.json()['access_token']
+
+        return {'Authorization': f'Bearer {access_token}'}
+
+    def check_response(self, response):
+        """ Check the response status code and return the json body if successful"""
+
+        if response.status_code == http.HTTPStatus.OK:
+            return response.json()
+        else:
+            raise SystemError(f"Response error: {response.text}")
+
+    def http_get(self, url):
+        """ HTTP GET request """
+        response = requests.get(url, headers=self.get_token_header())
+
+        self.log_response_info(response)
+
+        return response
+
+    def log_response_info(self, response):
+        """ log the response return code, headers and body """
+
+        logger.debug(f"Response status code: {response.status_code}")
+
+        for header, value in response.headers.items():
+            logger.debug(f"Response ('header:', '{header}:', '{value}')")
+
+        logger.debug(f"Response body: {response.text}")
+    
+
+    
+
--- src/main.py
+++ src/main.py
@@ -1,15 +1,12 @@
 import sys
 import traceback
+from azure_activity_logs import AzureActivityLogs
 import azure_api
-import azure_api_helper
-import os
-import json
+from azure_management import AzureManagement
+from azure_roles import AzureRoles
 import datetime_helper
 import role_helper
 from loguru import logger
-
-def format_object(obj):
-    return json.dumps(obj, indent=4)
 
 def validate_arguments():
     # Get the command-line arguments
@@ -17,8 +14,8 @@ def validate_arguments():
     logger.debug(f'Command-line arguments: {args}')
 
     if len(args) < 3:
-        raise Exception('Subscription ID and user name are required arguments')
-    
+        raise ValueError('Subscription ID and user name are required arguments')
+
     sub_id = args[1]
     user_name = args[2]
 
@@ -28,9 +25,9 @@ def validate_arguments():
             num_hours = int(args[3])
             if num_hours < 1:
                 raise ValueError('num_hours must be a positive integer')
-            
-        except ValueError:
-            raise Exception('num_hours must be a positive integer')
+
+        except ValueError as e:
+            raise ValueError('num_hours must be a positive integer') from e
     else:
         logger.debug('num_hours not specified, defaulting to 1')
         num_hours = 1
@@ -44,45 +41,85 @@ def validate_arguments():
 
     return sub_id, user_name, num_hours, rg_name
 
+def check_actions_used():
+    """ return the actions used by the user in the last n hours"""
+
+    sub_id, user_name, num_hours, rg_name = validate_arguments()
+
+    start_date, end_date = datetime_helper.get_last_n_hours(num_hours)
+    select = None
+
+    az = AzureActivityLogs()
+    activity_log = az.get_activity_log(sub_id, start_date, end_date, rg_name, select, user_name)
+
+    # extract the operation values
+    operations = [x['operationName']['value'] for x in activity_log['value']]
+
+    # sort and remove duplicates
+    operations = sorted(list(set(operations)))
+
+    logger.info(f'List of operations for {user_name} between {start_date} and {end_date}:')
+    logger.info(az.format_json_object(operations))
+
+    if operations and len(operations) > 0:
+        role = role_helper.create_role(operations)
+        logger.info('Sample role based on actions:')
+        logger.info(az.format_json_object(role))
+
+def check_role_assignments():
+    sub_id = sys.argv[1]
+
+    az = AzureRoles()
+
+    # list the role assignments for the subscription
+    role_assignments = az.get_role_assignments(sub_id)
+
+    properties = [x['properties'] for x in role_assignments['value']]
+
+    # extract the role assignment principalId and principalType from role_assignments
+    result = [{'principalId': x['principalId'], 
+                        'principalType': x['principalType'],
+                        'scope': x['scope'],
+                        'roleDefinitionId': x['roleDefinitionId']} for x in properties]
+                
+    logger.info(f"{len(result)} role assignments for subscription {sub_id}:")
+    logger.info(az.format_json_object(result))
+
+def check_subs_and_rgs():
+    """ Check the subscriptions and resource groups for the tenant"""
+
+    az = AzureManagement()
+
+    # list the subscriptions for the tenant
+    subscriptions = az.get_subscriptions()
+    logger.info(f"subscriptions: {az.format_json_object(subscriptions)}")
+    
+    for sub in subscriptions['value']:
+        sub_id = sub['subscriptionId']
+        resource_groups = az.get_resource_groups(sub_id)
+
+        # get the name, location and tags (if they exist) for each resource group
+        resource_groups = [{'name': x['name'], 'location': x['location'], 'tags': x['tags'] if 'tags' in x else None} for x in resource_groups['value']]
+        
+        logger.info(f"resource groups for subscription {sub_id}: {az.format_json_object(resource_groups)}")
+
 def main():
     try:
         logger.info("Starting up...")
 
-        sub_id, user_name, num_hours, rg_name = validate_arguments()
+        # check if the user has used any actions in the last n hours
+        # check_actions_used()
 
-        auth_headers = azure_api_helper.get_token_header(
-            os.environ.get("TENANT_ID"), 
-            os.environ.get("CLIENT_ID"), 
-            os.environ.get("CLIENT_SECRET"))
+        # check the role assignments for the subscription
+        # check_role_assignments()
 
-        azure = azure_api.AzureAPI(auth_headers)
-
-        start_date, end_date = datetime_helper.get_last_n_hours(num_hours)
-        select = None
-
-        activity_log = azure.get_activity_log(sub_id, start_date, end_date, rg_name, select, user_name)
-
-        # extract the operation values
-        operations = [x['operationName']['value'] for x in activity_log['value']]
-
-        logger.debug(f'List of operations:')
-        logger.debug(format_object(operations))
-
-        # sort and remove duplicates
-        operations = sorted(list(set(operations)))
-
-        logger.info(f'List of operations for {user_name} between {start_date} and {end_date}:')
-        logger.info(format_object(operations))
-
-        if operations and len(operations) > 0:
-            role = role_helper.create_role(operations)
-            logger.info('Sample role based on actions:')
-            logger.info(format_object(role))
-            
+        # check subs and resource groups
+        check_subs_and_rgs()
     except Exception as e:
         logger.error(f"Unexpected exception occurred: {e}")
         logger.error(f"Stack trace: {traceback.print_exc()}")
-    
+        sys.exit(1)
+
 # Check if the script is being run directly (not imported as a module)
 if __name__ == "__main__":
     main()
