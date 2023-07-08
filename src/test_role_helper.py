fcefe1906fea7cb360c1111dbc65f3255d65b80e
spectrum048k <107911114+spectrum048k@users.noreply.github.com>
2023-07-08 13:57:51 +1200

    reformatted files (#5)
    Co-authored-by: spectrum048k <>

--- src/test_role_helper.py
+++ src/test_role_helper.py
@@ -1,10 +1,17 @@
 from role_helper import create_role
 
+
 def test_create_role_with_actions():
-    role = create_role(['Microsoft.Resources/subscriptions/resourceGroups/read', 
-                 'Microsoft.Resources/subscriptions/resourceGroups/write', 
-                 'Microsoft.Resources/subscriptions/resourceGroups/delete'])
-    
+    role = create_role(
+        [
+            "Microsoft.Resources/subscriptions/resourceGroups/read",
+            "Microsoft.Resources/subscriptions/resourceGroups/write",
+            "Microsoft.Resources/subscriptions/resourceGroups/delete",
+        ]
+    )
+
     # check role contains the actions
-    assert 'Microsoft.Resources/subscriptions/resourceGroups/read' in role['properties']['permissions'][0]['actions']
-    
+    assert (
+        "Microsoft.Resources/subscriptions/resourceGroups/read"
+        in role["properties"]["permissions"][0]["actions"]
+    )
